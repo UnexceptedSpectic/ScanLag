58af5ff6382b87093cddc74d96b0804610a8b394
pbadzuh <pbadzuh@uncc.edu>
2019-11-11 14:15:58 -0500

    Update optimal_pixel_range.m
    Add number of colonies analyzed, change column titles, fix growth_mean calculation,

--- ConditionAnalysis/optimal_pixel_range.m
+++ ConditionAnalysis/optimal_pixel_range.m
@@ -1,7 +1,7 @@
 function [out, outStruct] = optimal_pixel_range(path_vectors)
-%optimal_pixel_range finds the pixel range along the colony area (pixels) vs time
-%curve that results in the minimum standard deviation
-%   strain_data is a string vector of directories to plates of the same strain
+% - optimal_pixel_range finds the pixel range along the colony area (pixels) vs time
+%   curve that results in the minimum standard deviation
+% - path_vectors is a string vector of directories to plates of the same strain
 
 %singles = 0:200;
 %doubles = 0:2:400;
@@ -16,7 +16,7 @@ for ind = 1:length(window_maxes)
     window_min = window_mins(ind);
     data = getAppearanceGrowth(path_vectors, window_min, window_max);
     stdev = std(rmoutliers(data.growth));
-    growth_mean = mean(data.growth);
+    growth_mean = mean(rmoutliers(data.growth));
     percent_std = stdev/growth_mean;
     if percent_std < min_percent_std
         min_percent_std = percent_std;
@@ -25,10 +25,11 @@ for ind = 1:length(window_maxes)
         ideal_mean = growth_mean;
         ideal_std = stdev;        
         growth = rmoutliers(data.growth);
+        n = length(rmoutliers(data.growth));
     end
 end
 
-out = ["min_std" "growth_mean" "pixel_min" "pixel_max" "percent_std"; ideal_std ideal_mean ideal_min ideal_max min_percent_std];
+out = ["min std" "growth mean (doubling time (days))" "pixel min" "pixel max" "percent std" "number of colonies used"; ideal_std ideal_mean ideal_min ideal_max min_percent_std n];
 outStruct = struct;
 outStruct.minStd = ideal_std;
 outStruct.growthMean = ideal_mean;
